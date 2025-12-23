62813cdbf6f939bc5bfb8ba4273299d7a1cdbdcd
Tobias Hegge <tobiaheg@stud.ntnu.no>
2025-12-23 18:25:59 +0100

    finished bookshelf class

--- book-manager/src/main/java/bookapp/Bookshelf.java
+++ book-manager/src/main/java/bookapp/Bookshelf.java
@@ -4,20 +4,19 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class Bookshelf {
+	private String owner;
 	private Set<Book> books = new HashSet<>();
+
+	public Bookshelf(String owner) {
+		this.owner = owner;
+	}
+
+	public String getOwner() {
+		return owner;
+	}
 
 	public Set<Book> getBooks() {
 		return books;
-	}
-
-	public int getTotalPages() {
-		int total = 0;
-
-		for (Book book : books) {
-			total += book.getPageNumber();
-		}
-
-		return total;
 	}
 
 	public Book getBookByTitle(String title) {
@@ -27,6 +26,9 @@ public class Bookshelf {
 			}
 		}
 
+		// Does not throw error because user is able to search for books and may make
+		// mistakes/typos
+		System.out.println("Couldn't find book in bookshelft with name: " + title);
 		return null;
 	}
 
@@ -34,20 +36,61 @@ public class Bookshelf {
 		for (Book book : books) {
 			if (book.getTitle() == bookToAdd.getTitle()) {
 				throw new IllegalArgumentException(
-						"Book with same title already in bookshelf: " + book.getTitle());
+						"Unable to add book: book with same title already in bookshelf: "
+								+ book.getTitle());
 
 			}
 		}
 
 		books.add(bookToAdd);
+		System.out.println("Successfully added book: " + bookToAdd.getTitle());
 	}
 
+	// To remove a book when only title is known, use getBookByTitle() -> Book
+	// method inside this method
 	public Book removeBook(Book book) {
+		if (book == null) {
+			System.out.println("Didn't remove book: input null.");
+			return null;
+		}
+
 		if (books.contains(book)) {
 			books.remove(book);
+			System.out.println("Successfully removed book: " + book.getTitle());
 			return book;
 		}
 
 		throw new IllegalArgumentException("Unable to remove book: book not in bookshelf: " + book.getTitle());
+	}
+
+	private int calculateTotalPages(Set<Book> booksToSum) {
+		int total = 0;
+
+		for (Book book : booksToSum) {
+			total += book.getPageNumber();
+		}
+
+		return total;
+	}
+
+	public int getTotalPages() {
+		return calculateTotalPages(books);
+	}
+
+	@SuppressWarnings("deprecation")
+	public Set<Book> getBooksByYear(int year) {
+		Set<Book> matchingBooks = new HashSet<>();
+
+		for (Book book : books) {
+			if (book.getReadDate().getYear() == year) {
+				matchingBooks.add(book);
+			}
+		}
+
+		return matchingBooks;
+	}
+
+	public int getTotalPagesByYear(int year) {
+		return calculateTotalPages(getBooksByYear(year));
 	}
 }
